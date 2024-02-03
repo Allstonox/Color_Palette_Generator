cdffb0b1f504f8c14f1162432976cde47ab91437
Allstonox <66976464+Allstonox@users.noreply.github.com>
2024-02-02 19:56:48 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -29,83 +29,14 @@ function generateNewPalette(paletteType = 'random') {
             </div>
     `;
     newPalette.addEventListener('mouseup', (e) => {
-        navigator.clipboard.writeText(e.target.innerText);
-        toggleCopyMessage(e.target);
+        toggleCopy(e.target, toggle = true);
     })
-    optionsPanel.innerHTML =`<ion-icon name="heart-outline"></ion-icon>
+    optionsPanel.innerHTML = `<ion-icon onclick="toggleLikes()" name="heart-outline"></ion-icon>
                             <ion-icon onclick="togglePaletteInformation(this.parentElement.previousElementSibling, 0, true)" name="ellipsis-horizontal"></ion-icon>`;
     newPaletteWrapper.appendChild(newPalette);
     newPaletteWrapper.appendChild(optionsPanel);
     paletteGrid.appendChild(newPaletteWrapper);
 }
-
-// function generatePaletteColors() {
-//     let palette = [];
-
-//     //Pick random starting color
-//     let startingHue = Math.floor(Math.random() * 360);
-//     let startingSaturation = mapValue(0, 100, 20, 80, Math.random() * 100);
-//     let startingBrightness = mapValue(0, 100, 35, 80, Math.random() * 100);
-//     palette.push({ hue: startingHue, saturation: startingSaturation, brightness: startingBrightness });
-
-//     //Define complementary color
-//     let complementaryHue = Math.abs(startingHue + 180 % 360);
-//     let complementarySaturation = startingSaturation;
-//     let complementaryBrightness = startingBrightness;
-//     palette.push({ hue: complementaryHue, saturation: complementarySaturation, brightness: complementaryBrightness });
-
-//     //Define other 3 colors by picking base color randomly between main and complementary
-//     for (let i = 0; i < 3; i++) {
-//         let colorChosen;
-//         if (Math.random() > 0.5) {
-//             colorChosen = {
-//                 hue: startingHue,
-//                 saturation: startingSaturation,
-//                 brightness: startingBrightness,
-//             }
-//         }
-//         else {
-//             colorChosen = {
-//                 hue: complementaryHue,
-//                 saturation: complementarySaturation,
-//                 brightness: complementaryBrightness,
-//             }
-//         }
-
-//         //Define random color based on base color chosen
-//         let randomHue;
-//         let randomSaturation;
-//         let randomBrightness;
-//         let colorChangeMin = 5;
-//         let colorChangeMax = 15;
-//         let colorChangeRange = 10;
-//         if (Math.random() > 0.5) {
-//             randomHue = colorChosen.hue + mapValue(0, 15, colorChangeMin, colorChangeMax, Math.random() * colorChangeRange);
-//         }
-//         else {
-//             randomHue = colorChosen.hue - mapValue(0, 15, colorChangeMin, colorChangeMax, Math.random() * colorChangeRange);
-//         }
-//         if (Math.random() > 0.5) {
-//             randomSaturation = colorChosen.saturation + mapValue(0, colorChangeRange, colorChangeMin, colorChangeMax, Math.random() * colorChangeRange);
-//         }
-//         else {
-//             randomSaturation = colorChosen.saturation - mapValue(0, colorChangeRange, colorChangeMin, colorChangeMax, Math.random() * colorChangeRange);
-//         }
-//         if (Math.random() > 0.5) {
-//             randomBrightness = colorChosen.brightness + mapValue(0, colorChangeRange, colorChangeMin, colorChangeMax, Math.random() * colorChangeRange);
-//         }
-//         else {
-//             randomBrightness = colorChosen.brightness - mapValue(0, colorChangeRange, colorChangeMin, colorChangeMax, Math.random() * colorChangeRange);
-//         }
-//         palette.push({ hue: randomHue, saturation: randomSaturation, brightness: randomBrightness });
-//     }
-//     palette.forEach((color) => {
-//         if (color.brightness > 60 || color.saturation > 70) color.textColor = 'black';
-//         else color.textColor = 'white';
-//         console.log(color);
-//     })
-//     return palette;
-// }
 
 function generatePaletteColors(paletteType = 'random') {
     let palette = [];
@@ -125,9 +56,8 @@ function generatePaletteColors(paletteType = 'random') {
     }
 
     palette.forEach((color) => {
-        if (color.brightness > 60 || color.saturation > 70) color.textColor = 'black';
+        if (color.saturation > 50 && color.brightness > 30) color.textColor = 'black';
         else color.textColor = 'white';
-        // console.log(color);
     })
     return palette;
 }
@@ -142,18 +72,32 @@ function toggleMenu(menuToToggle) {
 }
 
 let copyMessageToggled = false;
-function toggleCopyMessage(paletteClicked) {
-    let originalInnerHTML = paletteClicked.innerHTML;
-    let paletteTextColor = paletteClicked.children[0].style.color;
-    if (!copyMessageToggled) {
-        paletteClicked.innerHTML = `<ion-icon class="color-hexcode" name="checkmark-outline" style="width: 30px; height: 30px; color: ${paletteTextColor}"></ion-icon>`;
-        copyMessage.classList.toggle('slide-fade');
-        copyMessageToggled = true;
-        window.setTimeout(() => {
-            paletteClicked.innerHTML = originalInnerHTML;
+function toggleCopy(paletteClicked, toggle = false) {
+    if (toggle) {
+        let originalInnerHTML = paletteClicked.innerHTML;
+        let paletteTextColor = paletteClicked.children[0].style.color;
+        if (!copyMessageToggled) {
+            navigator.clipboard.writeText(paletteClicked.innerText);
+            paletteClicked.innerHTML = `<ion-icon class="color-hexcode" name="checkmark-outline" style="width: 30px; height: 30px; color: ${paletteTextColor}"></ion-icon>`;
             copyMessage.classList.toggle('slide-fade');
-            copyMessageToggled = false
-        }, 2000)
+            copyMessageToggled = true;
+            window.setTimeout(() => {
+                paletteClicked.innerHTML = originalInnerHTML;
+                copyMessage.classList.toggle('slide-fade');
+                copyMessageToggled = false
+            }, 2000)
+        }
+    }
+    else {
+        if (!copyMessageToggled) {
+            navigator.clipboard.writeText(paletteClicked.children[1].innerText);
+            paletteClicked.setAttribute('data-content', 'Copied!');
+            copyMessageToggled = true;
+            window.setTimeout(() => {
+                paletteClicked.setAttribute('data-content', 'Copy');
+                copyMessageToggled = false
+            }, 2000)
+        }
     }
 }
 
@@ -162,17 +106,40 @@ function toggleMenu(menuToToggle) {
     menu.classList.toggle('visible');
 }
 
-function togglePaletteInformation(paletteToDisplay, colorSelected = 0, toggle = false) {
+async function togglePaletteInformation(paletteToDisplay, colorSelected = 0, toggle = false) {
     let paletteInformation = document.querySelector('#palette-information');
     let paletteInformationBody = document.querySelector('#palette-information-body');
     let paletteItemsWrapper = document.querySelector('#palette-items-wrapper');
     let paletteItemSelected = document.querySelector('#palette-item-selected');
     let colors = [...paletteToDisplay.children];
     let textColor = colors[colorSelected].children[0].style.color;
+    let colorHexadecimalVal = colors[colorSelected].children[0].innerText;
+
+    let content = await fetchColorInformation(colorHexadecimalVal);
+    paletteInformationBody.innerHTML = `
+    <div data-content="Copy" onclick="toggleCopy(this)">
+        <p style="color: var(--light-text); font-size: 0.8rem; font-weight: 600;">NAME</p>
+        <p style="font-weight: 600; color: ${textColor};">${content.colorName}</p>
+    </div>
+    <div data-content="Copy" onclick="toggleCopy(this)">
+        <p style="color: var(--light-text); font-size: 0.8rem; font-weight: 600;">HEX</p>
+         <p style="font-weight: 600; color: ${textColor};">${content.colorHex}</p>
+    </div>
+    <div data-content="Copy" onclick="toggleCopy(this)">
+        <p style="color: var(--light-text); font-size: 0.8rem; font-weight: 600;">HSV</p>
+        <p style="font-weight: 600; color: ${textColor};">${content.colorHSB}</p>
+    </div>
+    <div data-content="Copy" onclick="toggleCopy(this)">
+        <p style="color: var(--light-text); font-size: 0.8rem; font-weight: 600;">HSL</p>
+        <p style="font-weight: 600; color: ${textColor};">${content.colorHSL}</p>
+    </div>
+    <div data-content="Copy" onclick="toggleCopy(this)">
+        <p style="color: var(--light-text); font-size: 0.8rem; font-weight: 600;">RGB</p>
+        <p style="font-weight: 600; color: ${textColor};">${content.colorRGB}</p>
+    </div>`
 
     paletteItemsWrapper.innerHTML = '';
-    console.log(colors);
-    for(let color of colors) {
+    for (let color of colors) {
         let colorToAdd = document.createElement('div');
         colorToAdd.classList.add('palette-item');
         colorToAdd.style.backgroundColor = `${color.style.backgroundColor}`;
@@ -182,33 +149,28 @@ function togglePaletteInformation(paletteToDisplay, colorSelected = 0, toggle = 
         })
         paletteItemsWrapper.appendChild(colorToAdd);
     }
-
     paletteInformationBody.style.backgroundColor = colors[colorSelected].style.backgroundColor;
-    paletteInformationBody.innerHTML = `
-    <div>
-        <p style="color: var(--light-text); font-size: 0.8rem; font-weight: 600;">NAME</p>
-        <p style="font-weight: 600; color: ${textColor}">Spicy Green</p>
-    </div>
-    <div>
-        <p style="color: var(--light-text); font-size: 0.8rem; font-weight: 600;">HEX</p>
-         <p style="font-weight: 600; color: ${textColor}">${colors[colorSelected].children[0].innerText}</p>
-    </div>
-    <div>
-        <p style="color: var(--light-text); font-size: 0.8rem; font-weight: 600;">HSB</p>
-        <p style="font-weight: 600; color: ${textColor}">25, 45, 234</p>
-    </div>
-    <div>
-        <p style="color: var(--light-text); font-size: 0.8rem; font-weight: 600;">HSL</p>
-        <p style="font-weight: 600; color: ${textColor}">${colors[colorSelected].style.backgroundColor}</p>
-    </div>
-    <div>
-        <p style="color: var(--light-text); font-size: 0.8rem; font-weight: 600;">RGB</p>
-        <p style="font-weight: 600; color: ${textColor}">100, 22, 212</p>
-    </div>`
-
     paletteItemSelected.style.color = textColor;
     paletteItemSelected.style.left = `calc(14% + ${colorSelected * 18}%)`
-    if(toggle) paletteInformation.classList.toggle('visible');
+    if (toggle) paletteInformation.classList.toggle('visible');
+}
+
+async function fetchColorInformation(color) {
+    // construct the API url with the color parameter
+    let url = `https://www.thecolorapi.com/id?hex=${color}`;
+
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        let name = data.name.value;
+        let hex = data.hex.value;
+        let hsb = data.hsv.value;
+        let hsl = data.hsl.value;
+        let rgb = data.rgb.value;
+        return { colorName: name, colorHex: hex, colorHSB: hsb, colorHSL: hsl, colorRGB: rgb };
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 function selectPaletteType(paletteTypeChosen) {
@@ -217,7 +179,7 @@ function selectPaletteType(paletteTypeChosen) {
     switch (paletteTypeChosen) {
         case 'complementary':
             for (let i = 0; i < paletteNumber; i++) {
-                generateNewPalette();
+                generateNewPalette('complementary');
                 currentPaletteType = 'complementary';
                 paletteMenu.innerHTML = `<li onclick="selectPaletteType('complementary')">Complementary <ion-icon name="checkmark-outline" style="color: grey;"></ion-icon></li>
                 <li onclick="selectPaletteType('monochromatic')">Monochromatic</li>
